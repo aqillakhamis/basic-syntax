87cfb832bea827ee8231f53f9bc170a235f29868
Nurulaqilla Khamis <nurulaqilla.k@umk.edu.my>
2021-11-07 09:23:14 +0800

    Add files via upload

---
+++ exercise2.c
@@ -0,0 +1,16 @@
+//documentation section
+/*Nurulaqilla Khamis, 1/11/2021, Description: exercise 2 */
+
+//pre-processor section 
+#include<stdio.h>
+
+//global variable section 
+
+
+//main function section 
+int main(){
+    float myrExchange = 4.10;
+    printf("Malaysian Ringgit exchange rate in US dollar is RM %.3f", myrExchange);
+}
+
+//user-defined section 
